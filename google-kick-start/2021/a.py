f1f4a95064ce3bddd73df4375e41e9eb5feb7809
Brian Strauch <bstrauch24@gmail.com>
2021-03-23 18:32:34 -0500

    Round A 2021 Problem A

--- google-kick-start/2021/a.py
+++ google-kick-start/2021/a.py
@@ -10,10 +10,17 @@ def read_int():
 def read_ints():
     return [int(x) for x in input().split()]
 
-def solve():
-    return 0
+def solve(n, k, s):
+    goodness = 0
+    for i in range(n // 2):
+        if s[i] != s[n - 1 - i]:
+            goodness += 1
+    return abs(k - goodness)
 
 tt = read_int()
 for cc in range(1, tt + 1):
-    ans = solve()
+    n, k = read_ints()
+    s = read_str()
+
+    ans = solve(n, k, s)
     print(f'Case #{cc}: {ans}')
